faf804f899ae2c62e0981d6dc9c355f54e3d246c
bachngocng <nnbach97@gmail.com>
2021-12-22 13:37:11 +0700

    done

---
+++ myCircle.js
@@ -0,0 +1,30 @@
+class MyCircle {
+    radius;
+    color;
+
+
+    constructor(radius,color) {
+        this.radius = radius;
+        this.color = color;
+
+    }
+    getRadius() {
+        return this.radius
+    }
+
+    getColor() {
+        return this.color
+    }
+
+
+    getArea() {
+        return this.radius*this.radius*Math.PI
+    }
+
+    setRadius(radius) {
+        this.radius = radius;
+    }
+
+
+
+}
